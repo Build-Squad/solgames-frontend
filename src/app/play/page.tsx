ad75cadd9726ff7a0cb3a803889b2207415b89cd
ParikshitWebllisto <parikshit.s@webllisto.com>
2024-06-20 01:31:24 +0530

    add: play route styling

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,8 +1,30 @@
 import { Box } from "@mui/material";
 import React from "react";
+import World_map from "../../assets/World_map.svg";
 
 type Props = {};
 
 export default function Play({}: Props) {
-  return <Box sx={{ backgroundColor: "#252525", height: "100vh" }}></Box>;
+  return (
+    <Box
+      sx={{
+        backgroundColor: "#252525",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          height: "80%",
+          width: "100%",
+          backgroundImage: `url(${World_map.src})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      ></Box>
+    </Box>
+  );
 }
