89836b1b9a889db03e32debf448d9c1281eed4e6
ParikshitWebllisto <parikshit.s@webllisto.com>
2024-07-05 16:17:31 +0530

    refactor: component and design

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,6 +1,5 @@
 import { Box } from "@mui/material";
 import React from "react";
-import World_map from "../../assets/World_map.svg";
 import Chessboard from "@/components/chessboard/chessboard";
 
 type Props = {};
@@ -9,25 +8,12 @@ export default function Play({}: Props) {
   return (
     <Box
       sx={{
-        backgroundColor: "#252525",
-        height: "100vh",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
       }}
     >
-      <Box
-        sx={{
-          height: "fit-content",
-          width: "100%",
-          backgroundImage: `url(${World_map.src})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <Chessboard />
-      </Box>
+      <Chessboard />
     </Box>
   );
 }
