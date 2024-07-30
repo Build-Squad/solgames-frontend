da6c1be2010f62865567af8dc9aedc5814200fe5
parikshitlfj12 <parikshitlfj@gmail.com>
2024-07-30 21:38:39 +0530

    chore: creating actions and blinks to share

--- src/app/api/actions.json/route.ts
+++ src/app/api/actions.json/route.ts
@@ -1,12 +1,11 @@
 import { ACTIONS_CORS_HEADERS, ActionsJson } from "@solana/actions";
 
 export const GET = (req: Request) => {
-  console.log("GET called");
   const payload: ActionsJson = {
     rules: [
       {
         pathPattern: "/join",
-        apiPath: "api/actions/memo",
+        apiPath: "api/actions/join",
       },
     ],
   };
